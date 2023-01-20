f5b20f4a3627e3b129c522710d70e1807c9a6bea
tankche1 <tankche2@gmail.com>
2023-01-20 11:58:19 -0500

    My

--- configs/mtl_swin/swin_retinanet_uppernet.py
+++ configs/mtl_swin/swin_retinanet_uppernet.py
@@ -1,14 +1,27 @@
 _base_ = [
     # '../_base_/models/retinanet_r50_fpn.py',
     '../_base_/datasets/imagenet_coco_ade20k_swin.py',
-    # '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+    '../_base_/schedules/imgnet_ade20k_coco_SGD.py', 
+    '../_base_/default_runtime.py'
 ]
-# pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
 
+
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+runner = dict(type='EpochBasedRunner', max_epochs=50)
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
+optimizer_config = dict()
+
+# mmcls
+evaluation = dict(interval=1, metric='accuracy')
+# mmseg
+evaluation = dict(interval=16000, metric='mIoU', pre_eval=True)
+# mmdet
+evaluation = dict(interval=1, metric='bbox')
 
 # model settings
 model = dict(
-    type='',
+    type='ImageMTLearner',
     backbone=dict(
                 _delete_=True,
                 type='SwinTransformer',
@@ -106,108 +119,108 @@ model = dict(
                 max_per_img=100)
         ),
         dict(
-        type='EncoderDecoder',
-        pretrained=None,
-        decode_head=dict(
-            type='UPerHead',
-            in_channels=[96, 192, 384, 768],
-            in_index=[0, 1, 2, 3],
-            pool_scales=(1, 2, 3, 6),
-            channels=512,
-            dropout_ratio=0.1,
-            num_classes=19,
-            norm_cfg=dict(type='SyncBN', requires_grad=True),
-            align_corners=False,
-            loss_decode=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
-        auxiliary_head=dict(
-            type='FCNHead',
-            in_channels=384,
-            in_index=2,
-            channels=256,
-            num_convs=1,
-            concat_input=False,
-            dropout_ratio=0.1,
-            num_classes=19,
-            norm_cfg=norm_cfg,
-            align_corners=False,
-            loss_decode=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
-        # model training and testing settings
-        train_cfg=dict(),
-        test_cfg=dict(mode='whole')
-        ),
+            type='EncoderDecoder',
+            pretrained=None,
+            decode_head=dict(
+                type='UPerHead',
+                in_channels=[96, 192, 384, 768],
+                in_index=[0, 1, 2, 3],
+                pool_scales=(1, 2, 3, 6),
+                channels=512,
+                dropout_ratio=0.1,
+                num_classes=19,
+                norm_cfg=dict(type='SyncBN', requires_grad=True),
+                align_corners=False,
+                loss_decode=dict(
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+            auxiliary_head=dict(
+                type='FCNHead',
+                in_channels=384,
+                in_index=2,
+                channels=256,
+                num_convs=1,
+                concat_input=False,
+                dropout_ratio=0.1,
+                num_classes=19,
+                norm_cfg=norm_cfg,
+                align_corners=False,
+                loss_decode=dict(
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+            # model training and testing settings
+            train_cfg=dict(),
+            test_cfg=dict(mode='whole')
+            ),
 
         ]
 )
 
 
-model = dict(
-    backbone=dict(
-        # init_cfg=dict(type='Pretrained', checkpoint=None),
-        embed_dims=96,
-        depths=[2, 2, 6, 2],
-        num_heads=[3, 6, 12, 24],
-        window_size=7,
-        use_abs_pos_embed=False,
-        drop_path_rate=0.3,
-        patch_norm=True),
-    decode_head=dict(in_channels=[96, 192, 384, 768], num_classes=150),
-    auxiliary_head=dict(in_channels=384, num_classes=150))
+# model = dict(
+#     backbone=dict(
+#         # init_cfg=dict(type='Pretrained', checkpoint=None),
+#         embed_dims=96,
+#         depths=[2, 2, 6, 2],
+#         num_heads=[3, 6, 12, 24],
+#         window_size=7,
+#         use_abs_pos_embed=False,
+#         drop_path_rate=0.3,
+#         patch_norm=True),
+#     decode_head=dict(in_channels=[96, 192, 384, 768], num_classes=150),
+#     auxiliary_head=dict(in_channels=384, num_classes=150))
 
-norm_cfg = dict(type='SyncBN', requires_grad=True)
-backbone_norm_cfg = dict(type='LN', requires_grad=True)
-model = dict(
-    type='EncoderDecoder',
-    pretrained=None,
-    backbone=dict(
-        type='SwinTransformer',
-        pretrain_img_size=224,
-        embed_dims=96,
-        patch_size=4,
-        window_size=7,
-        mlp_ratio=4,
-        depths=[2, 2, 6, 2],
-        num_heads=[3, 6, 12, 24],
-        strides=(4, 2, 2, 2),
-        out_indices=(0, 1, 2, 3),
-        qkv_bias=True,
-        qk_scale=None,
-        patch_norm=True,
-        drop_rate=0.,
-        attn_drop_rate=0.,
-        drop_path_rate=0.3,
-        use_abs_pos_embed=False,
-        act_cfg=dict(type='GELU'),
-        norm_cfg=backbone_norm_cfg),
-    decode_head=dict(
-        type='UPerHead',
-        in_channels=[96, 192, 384, 768],
-        in_index=[0, 1, 2, 3],
-        pool_scales=(1, 2, 3, 6),
-        channels=512,
-        dropout_ratio=0.1,
-        num_classes=19,
-        norm_cfg=norm_cfg,
-        align_corners=False,
-        loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
-    auxiliary_head=dict(
-        type='FCNHead',
-        in_channels=384,
-        in_index=2,
-        channels=256,
-        num_convs=1,
-        concat_input=False,
-        dropout_ratio=0.1,
-        num_classes=19,
-        norm_cfg=norm_cfg,
-        align_corners=False,
-        loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
-    # model training and testing settings
-    train_cfg=dict(),
-    test_cfg=dict(mode='whole'))
+# norm_cfg = dict(type='SyncBN', requires_grad=True)
+# backbone_norm_cfg = dict(type='LN', requires_grad=True)
+# model = dict(
+#     type='EncoderDecoder',
+#     pretrained=None,
+#     backbone=dict(
+#         type='SwinTransformer',
+#         pretrain_img_size=224,
+#         embed_dims=96,
+#         patch_size=4,
+#         window_size=7,
+#         mlp_ratio=4,
+#         depths=[2, 2, 6, 2],
+#         num_heads=[3, 6, 12, 24],
+#         strides=(4, 2, 2, 2),
+#         out_indices=(0, 1, 2, 3),
+#         qkv_bias=True,
+#         qk_scale=None,
+#         patch_norm=True,
+#         drop_rate=0.,
+#         attn_drop_rate=0.,
+#         drop_path_rate=0.3,
+#         use_abs_pos_embed=False,
+#         act_cfg=dict(type='GELU'),
+#         norm_cfg=backbone_norm_cfg),
+#     decode_head=dict(
+#         type='UPerHead',
+#         in_channels=[96, 192, 384, 768],
+#         in_index=[0, 1, 2, 3],
+#         pool_scales=(1, 2, 3, 6),
+#         channels=512,
+#         dropout_ratio=0.1,
+#         num_classes=19,
+#         norm_cfg=norm_cfg,
+#         align_corners=False,
+#         loss_decode=dict(
+#             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+#     auxiliary_head=dict(
+#         type='FCNHead',
+#         in_channels=384,
+#         in_index=2,
+#         channels=256,
+#         num_convs=1,
+#         concat_input=False,
+#         dropout_ratio=0.1,
+#         num_classes=19,
+#         norm_cfg=norm_cfg,
+#         align_corners=False,
+#         loss_decode=dict(
+#             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+#     # model training and testing settings
+#     train_cfg=dict(),
+#     test_cfg=dict(mode='whole'))
 
 
 
--- mmmtl/models/__init__.py
+++ mmmtl/models/__init__.py
@@ -2,13 +2,14 @@
 from .backbones import *  # noqa: F401,F403
 from .builder import (BACKBONES, CLASSIFIERS, HEADS, LOSSES, NECKS,
                       build_backbone, build_classifier, build_head, build_loss,
-                      build_neck)
+                      build_neck, build_mtlearner)
 from .classifiers import *  # noqa: F401,F403
 from .heads import *  # noqa: F401,F403
 from .losses import *  # noqa: F401,F403
 from .necks import *  # noqa: F401,F403
+from .mtlearner import *
 
 __all__ = [
     'BACKBONES', 'HEADS', 'NECKS', 'LOSSES', 'CLASSIFIERS', 'build_backbone',
-    'build_head', 'build_neck', 'build_loss', 'build_classifier'
+    'build_head', 'build_neck', 'build_loss', 'build_classifier', 'build_mtlearner'
 ]
